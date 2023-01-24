fe634d2285da3b3ce568d214d562b08ee2849a30
AndreiRookie <andrey.idem@gmail.com>
2023-01-24 18:53:55 +0400

    methods add, delete, search extracted

--- src/Main.java
+++ src/Main.java
@@ -13,8 +13,8 @@ public class Main {
 
         while (true) {
             System.out.println("Выберите операцию:\n1. Добавить товар\n" +
-                            "2. Показать список покупок\n3. Удалить товар\n" +
-                            "4. Поиск\n5. Выход");
+                    "2. Показать список покупок\n3. Удалить товар\n" +
+                    "4. Поиск\n5. Выход");
 
             String operationNumber = scanner.nextLine();
 
@@ -22,80 +22,44 @@ public class Main {
                 case "1" -> {
                     System.out.println("Какую покупку хотите добавить?");
                     String purchase = scanner.nextLine();
-                    shoppingList.add(purchase.trim());
-                    System.out.println("Итого в списке покупок: " + shoppingList.size());
+                    if (addProduct(purchase, shoppingList)) {
+                        System.out.println("Итого в списке покупок: " + shoppingList.size());
+                    }
                 }
                 case "2" -> {
                     System.out.println("Список покупок:");
-                    if (shoppingList.isEmpty()) {
-                        System.out.println("Empty");
-                    } else {
-                        System.out.println(shoppingListToString(shoppingList));
-                    }
+                    System.out.println(shoppingListToString(shoppingList));
                 }
                 case "3" -> {
                     if (shoppingList.isEmpty()) {
-                        System.out.println("Empty");
+                        System.out.println("shoppingList is empty");
                         break;
                     }
-
-                    System.out.println("Список покупок:");
-                    System.out.println(shoppingListToString(shoppingList));
+                    System.out.print("Список покупок:\n"+ shoppingListToString(shoppingList));
                     System.out.println("Какую хотите удалить? Введите номер или название");
-
                     String itemToDelete = scanner.nextLine();
 
                     try {
-                        int itemAsNumber = Integer.parseUnsignedInt(itemToDelete);
-
-                        if (itemAsNumber <= shoppingList.size()) {
-                            String deletedItem = shoppingList.get(itemAsNumber -1);
-                            shoppingList.remove(itemAsNumber - 1);
-                            System.out.println("Покупка \"" + deletedItem  +"\" удалена, список покупок:");
-                            System.out.println(shoppingListToString(shoppingList));
+                        int itemNumber = Integer.parseUnsignedInt(itemToDelete);
+                        if (deleteProduct(itemNumber - 1, shoppingList)) {
+                            System.out.println("Покупка №" + itemNumber + " удалена, список покупок:");
                         } else {
-                            System.out.println("Such item absents");
+                            System.out.println("нет такого товара");
                         }
                     } catch (NumberFormatException e) {
-                        boolean wasDeleted = false;
-                        for (Iterator<String> iterator = shoppingList.iterator(); iterator.hasNext();) {
-                            String nextItem = iterator.next();
-                            if (itemToDelete.trim().equalsIgnoreCase(nextItem)) {
-                                iterator.remove();
-                                System.out.println("Покупка \"" + nextItem  +"\" удалена, список покупок:");
-                                if (shoppingList.isEmpty()) {
-                                    System.out.println("Empty");
-                                    wasDeleted = true;
-                                    break;
-                                }
-                                System.out.println(shoppingListToString(shoppingList));
-                                wasDeleted = true;
-                                break;
-                            }
+
+                        if (deleteProduct(itemToDelete, shoppingList)) {
+                            System.out.println("Покупка \"" + itemToDelete + "\" удалена, список покупок:");
+                        } else {
+                            System.out.println("нет такого товара");
                         }
-                        if (!wasDeleted) {System.out.println("Such item absents");}
                     }
                 }
                 case "4" -> {
-                    if (shoppingList.isEmpty()) {
-                        System.out.println("Empty");
-                        break;
-                    }
-
-                    List<String> foundItems = new ArrayList<>();
-
                     System.out.println("Введите текст для поиска:");
                     String query = scanner.nextLine();
-                    String queryLower = query.toLowerCase();
-
-                    for (String item : shoppingList) {
-                        String itemLower = item.toLowerCase();
-                        if (itemLower.contains(queryLower)) {
-                            foundItems.add(item);
-                        }
-                    }
-                    System.out.println("Найдено:");
-                    System.out.println(shoppingListToString(foundItems));
+                    List<String> foundItems = searchProduct(query, shoppingList);
+                    System.out.println("Найдено:\n" + shoppingListToString(foundItems));
                 }
                 case "5" -> {
                     System.out.println("Bye bye");
@@ -105,14 +69,51 @@ public class Main {
             }
         }
     }
+    public static boolean deleteProduct(String item, List<String> shoppingList) {
+        for (Iterator<String> iterator = shoppingList.iterator(); iterator.hasNext(); ) {
+            if (item.trim().equalsIgnoreCase(iterator.next())) {
+                iterator.remove();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean deleteProduct(int index, List<String> shoppingList) {
+        if (0 <= index && index < shoppingList.size()) {
+            shoppingList.remove(index);
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean addProduct(String product, List<String> shoppingList) {
+        return shoppingList.add(product.trim());
+    }
+
+    private static List<String> searchProduct(String query, List<String> shoppingList) {
+        List<String> foundProducts = new ArrayList<>();
+
+        if (shoppingList.isEmpty()) {
+            return foundProducts;
+        } else {
+            String queryLower = query.toLowerCase();
+            for (String item : shoppingList) {
+                String itemLower = item.toLowerCase();
+                if (itemLower.contains(queryLower)) {
+                    foundProducts.add(item);
+                }
+            }
+        }
+        return foundProducts;
+    }
 
     private static String shoppingListToString(List<String> list) {
         if (list.isEmpty()) {
             return "";
         }
-
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i< list.size(); i++) {
+        for (int i = 0; i < list.size(); i++) {
             sb.append(i + 1).append(". ").append(list.get(i)).append(".\n");
         }
         return sb.toString();
